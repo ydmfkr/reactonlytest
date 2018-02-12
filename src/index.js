c254071651722231043deb66639cb46f5f49b384
fahad <d_mohajir@hotmail.com>
2018-02-12 13:11:31 +0400

    Test

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+//heres something i changed from the lappy
+
+let x = 100;
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
